920613d163e9bfb771de05db1c5f10959989dd02
aryan <aryansood972@gmail.com>
2024-04-29 10:30:33 +0200

    -

---
+++ Quadcode_Esp32/src/Comm/Data_rx.h
@@ -0,0 +1,8 @@
+#include <SPI.h>
+#include <RF24.h>   
+
+class Data_rx
+{
+public:
+    
+};
--- common/Kalman_Filter/EKF_attitude_estimation.h
+++ common/Kalman_Filter/EKF_attitude_estimation.h
@@ -7,8 +7,23 @@ Quaternion q_rot_dot(1,0,0,0);
 
 //I need covariance matrixm, acc, magn, barometer, gyro. Acc to have a point to which refer, plus magentometer.
 
-void Ukf_Attitude()
+void Attitude_Estimation()
 {
     
+}
+
+void Ukf_Attitude_Acc_On()
+{
+       
+}
+
+void Ukf_Attitude_Acc_Off()
+{
+
+}
+
+void Ukf_Full_State()
+{
+
 }
 
